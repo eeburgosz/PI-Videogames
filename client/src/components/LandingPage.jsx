b2affb71f153821404a461d9694229e78d50a9b0
Ernesto <hdst.bur@gmail.com>
2022-08-02 23:39:42 -0300

    CSS Create - 902%listo (más o menos)

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -7,7 +7,7 @@ export const LandingPage = () => {
   return (
     <>
       <div className="containerLanding">
-        <h2 className="texto"> Bienvenidos a</h2>
+        <h2 className="texto"> Welcome to</h2>
         <h1 className="texto2">EB Games</h1>
         <Link to="/home">
           <button className="boton">Entrar</button>
